6e9427476a893838d391c12513b086f23a29958e
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-01 20:43:51 +0530

    yay

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,9 +1,9 @@
-import {Entity, PrimaryGeneratedColummn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColummn()
+    @PrimaryGeneratedColumn()
     'id': number;
 
     @Column()
