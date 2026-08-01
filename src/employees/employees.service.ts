2d6ac88e16f0ac6dc458807c7069d34ebb78eb1a
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-02 01:13:17 +0530

    yayy

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from './employees.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class EmployeesService {
     constructor(
         @InjectRepository(Employee) // tells what entity to be injeced in this service
+        private employeeRepository: Repository <Employee> //Repository tells on what entity crud rules are to applied
     ) {}
 }
