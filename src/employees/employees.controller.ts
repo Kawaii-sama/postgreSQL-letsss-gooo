3b91ad8c8298335b3a3b62fe3804249b1c4c71cc
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-02 01:30:04 +0530

    yayy

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { EmployeesService } from './employees.service';
+import { Employee } from './employees.entity';
 
 @Controller('employees')
-export class EmployeesController {}
+export class EmployeesController {
+    constructor (private readonly employeesSerivce: EmployeesService) {}
+
+    @Post()
+    async createEmployee (@Body() body: Partial<Employee>) : Promise<Employee> {
+        return this.employeesSerivce.create(body);
+    }
+}
