9b154ffd5d7e9986761445c549a0a0e5b5c629ba
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-02 19:00:49 +0530

    building nest

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Employee } from './employees.entity';
 
@@ -28,4 +28,13 @@ export class EmployeesController {
     ) : Promise<Employee> {
         return this.employeesSerivce.update(id,body);
     }
+
+    @Delete(':id')
+    async deleteEmployee(
+        @Param('id') id : number
+    ) : Promise<{ message : string}> {
+
+        return this.employeesSerivce.delete(id);
+    }
+
 }
