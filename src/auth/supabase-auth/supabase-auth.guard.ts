708e639d4fe5e7d3e5f20d23df45742db6f1027c
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-05 18:17:31 +0530

    building nest

--- src/auth/supabase-auth/supabase-auth.guard.ts
+++ src/auth/supabase-auth/supabase-auth.guard.ts
@@ -1,13 +1,21 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import * as jwt from 'jsonwebtoken';
 import { Request } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SupabaseAuthGuard implements CanActivate {
+
+  constructor(private configService : ConfigService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.headers['authorization'];
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+      throw new UnauthorizedException('No token provided');
+    }
     return true;
   }
 }
