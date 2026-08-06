75d6b3d0a84cfb9297781b39b95784c08ec1ae2c
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-06 22:07:15 +0530

    building nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeesModule } from './employees/employees.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -20,7 +21,8 @@ import { MongooseModule } from '@nestjs/mongoose';
       synchronize: true
     }),
     UserModule,
-    EmployeesModule],
+    EmployeesModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
