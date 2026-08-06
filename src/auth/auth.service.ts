ae4f9f15fc8b3016d9ba51e0524400a1c57a93be
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-07 01:46:30 +0530

    building nest

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './user.schema';
 import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(
-        @InjectModel(User.name) private userModel: Model <UserDocument>
+        @InjectModel(User.name)
+        private userModel: Model <UserDocument>,
+        private jwtService : JwtService
     ) {}
+
+    async signup( email : string, password : string) {
+        const hash = await bcrypt.hash(password, 10);
+    }
 }
