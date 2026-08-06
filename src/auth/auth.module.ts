24da1d4189f18a60f63bdb583452bac8799b31fc
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-07 00:54:35 +0530

    building nest

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { User, UserSchema } from './user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService} from '@nestjs/config';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema}]),
+    JwtModule.registerAsync({imports : [ConfigModule], inject : [ConfigService]})
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
