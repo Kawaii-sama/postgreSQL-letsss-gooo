34c0538556ff62c0d5acfb2b8edb4c8974012775
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-06 22:14:51 +0530

    building nest

--- src/auth/user.schema.ts
+++ src/auth/user.schema.ts
@@ -2,4 +2,12 @@ import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
-export class User{}
+
+@Schema()
+export class User{
+    @Prop({required: true, unique: true})
+    email : string;
+
+    @Prop({required: true})
+    password: string;
+} 
