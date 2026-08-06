fc91b42bf9e83f2eebdb31d652d602babd552693
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-07 01:31:02 +0530

    building nest

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,6 +3,8 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 
+
+@Injectable()
 export class JwtStrategy extends PassportStrategy (Strategy) {
     constructor ( configService: ConfigService) {
         super({
