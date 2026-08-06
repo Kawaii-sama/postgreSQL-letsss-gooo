9294f0fe1708e02e86b217da76c04162c30f40ed
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-07 01:23:03 +0530

    building nest

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,4 +3,11 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 
-export class JwtStrategy extends PassportStrategy (Strategy) {}
+export class JwtStrategy extends PassportStrategy (Strategy) {
+    constructor ( configService: ConfigService) {
+        super({
+            jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey : configService.get<string> ('MY_JWT_SECRET')
+        });
+    }
+}
