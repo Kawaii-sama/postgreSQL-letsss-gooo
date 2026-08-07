617ca3af121a5c57fa199a1094c7264cd318afae
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-07 19:11:31 +0530

    building nest

--- src/book/dto/create-book.input.ts
+++ src/book/dto/create-book.input.ts
@@ -2,7 +2,7 @@ import {InputType, Field} from '@nestjs/graphql';
 import { IsNotEmpty, IsString} from 'class-validator';
 
 @InputType()
-export class CreateBookinput {
+export class CreateBookInput {
     @Field()
     @IsString()
     @IsNotEmpty()
