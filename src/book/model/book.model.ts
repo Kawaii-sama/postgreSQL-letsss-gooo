6776cafedf65a8814cecc83c4e015ff0e6965919
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-07 18:53:56 +0530

    building nest

--- src/book/model/book.model.ts
+++ src/book/model/book.model.ts
@@ -1,10 +1,15 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Types } from 'mongoose';
 
 @Schema()
 @ObjectType()
 
 export class Book extends Document {
-    
+    @Field(() => ID)
+    //declare readonly _id : Types.ObjectId;
+    declare readonly _id : string;
+
+
 }
